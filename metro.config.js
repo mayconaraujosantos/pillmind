a348281125c9f05b9a395edf7ec28670b4d7953c
mayconaraujosantos <mv.maycon.araujo.santos@gmail.com>
2025-12-27 17:53:20 -0300

    chore: ignore .trunk folder in Metro and Git

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,19 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Ignore problematic directories
+config.watchFolders = config.watchFolders || [];
+config.resolver = {
+  ...config.resolver,
+  blockList: [
+    ...(config.resolver?.blockList || []),
+    /\.trunk\/.*/,
+  ],
+};
+
+// Exclude .trunk from watcher
+config.watchFolders = config.watchFolders.filter(
+  (folder) => !folder.includes('.trunk')
+);
+
 module.exports = config;
